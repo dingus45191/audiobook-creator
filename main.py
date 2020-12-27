8b99caccb3e7f4d3181c60d90bf534b2ac3e57b6
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-27 20:26:21 +0530

    works now thanks to a bunch of awesome dudes

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ speaker = pyttsx3.init()
  
 book = "todayndtomorrow.pdf"
 with pdfplumber.open(book) as pdf:
+    # for reading one page or any specific page
     page = pdf.pages[74]
     print(page.extract_text())
     speaker.say(page.extract_text())
     speaker.runAndWait()
+
+# for reading the whole pdf
+    pages = pdf.pages
+    for i,pg in enumerate(pages):
+        content = pages[i].extract_text()
+        print(content)
+        speaker.say(content)
+        speaker.runAndWait()
