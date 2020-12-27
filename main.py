01dbefad908e6eea996691290bf13456f25b53dc
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-12-27 20:12:24 +0530

    works now thanks to an awesome guy at reddit

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
+import pdfplumber
 import pyttsx3
-import PyPDF2
-
-book = open('art-of-the-deal.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
-print(pages)
-
+ 
 speaker = pyttsx3.init()
-
-for num in range(0, pages):
-    page = pdfReader.getPage(4)
-    text = page.extractText()
-    speaker.say(text)
+ 
+book = "todayndtomorrow.pdf"
+with pdfplumber.open(book) as pdf:
+    page = pdf.pages[74]
+    print(page.extract_text())
+    speaker.say(page.extract_text())
     speaker.runAndWait()
-    
-book.close()    
-    
